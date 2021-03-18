09cb56e75f043dbf5cae57651e244f7b9b61cd1b
Anastasya-Klimenko <80512099+Anastasya-Klimenko@users.noreply.github.com>
2021-03-18 15:06:14 +0300

    Add files via upload

---
+++ Post.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8" /> 
+</head>
+<body>
+<?php
+ $form = 
+ '<form action="Post.php" method="POST" >
+ <p><span>Логин:</span> 
+ <span ><input type="text" size="20" 
+name="login" /></span></p>
+ <p><span>Пароль:</span>
+ <span > <input type="password" 
+name="password" /></span></p>
+ <input type="submit" 
+name="submit_form" value="Отправить" />
+ </form>
+ </body>
+ </html>';
+ 
+ if (isset($_POST['submit_form'])) {
+ if ($_POST['login'] != 'admin' ||
+$_POST['password'] != 'password') {
+ echo "<h2>Неправильный ввод данных!</h2>".$form; 
+ die();
+ } 
+ } else {
+ echo $form;
+ die(); 
+ } 
+?>
+<h1> Добро пожаловать на наш сайт!</h1>
+</body> 
---
+++ Utf.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8" /> 
+<style> <!--выравниваем форму по центру экрана--->
+ form {
+ width: 290px;
+ border: 1px solid black;
+ }
+</style>
+</head>
+<body>
+<?php
+ if (isset($_GET['submit_form'])) {
+ echo "<h2>Ответ сервера:</h2>";
+ echo "Ваше имя:<strong>
+".$_GET['user_name']."</strong></br>";
+ echo "И вы родились в <strong>
+".$_GET['user_year']."</strong> году!"; 
+ }
+?>
+<form action="Utf.php" method="get" >
+<p><span>Ваше имя:</span> 
+<span ><input type="text" size="20" name="user_name" /></span>
+</p>
+<p><span>Год рождения:</span><span> 
+<input type="number" name="user_year"/></span>
+</p>
+<input type="submit" name="submit_form" value="Отправить" />
+</form>
+</body> 
+</html>
